0c018cafebfca35f7498c303422912abd18d42a4
mhilgefort <mhilgefort@hwp06.informatik.uni-osnabrueck.de>
2019-03-06 13:32:30 +0100

    line detection in ndvi, added pcl creation ( not working )

--- reactive_navigation/src/imgSubscriber.cpp
+++ reactive_navigation/src/imgSubscriber.cpp
@@ -7,6 +7,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <boost/array.hpp>
+
 #include <librealsense2/rs.hpp>
 
 #include <dynamic_reconfigure/DoubleParameter.h>
@@ -17,7 +21,19 @@
 using namespace std;
 using namespace cv;
 using namespace rs2;
+using namespace pcl;
 
+
+
+
+
+Mat srcImage;
+Mat irImage;
+Mat depthImage;
+Mat splitImage;
+ros::Publisher pointcloud_publisher;
+int minLineLength = 175;
+int maxLineGap = 100;
 
 
 void disableEmitter(){
@@ -36,11 +52,13 @@ void disableEmitter(){
 	ros::service::call("/joint_commander/set_parameters", srv_req, srv_resp);
 }
 
-Mat srcImage;
-Mat irImage;
-Mat splitImage;
-int minLineLength = 175;
-int maxLineGap = 100;
+
+
+vector<Vec4i> detectFeatures(){
+
+
+
+}
 
 
 
@@ -51,138 +69,81 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
     srcImage = cv_bridge::toCvShare(msg, "bgr8")->image;
     splitImage = cv_bridge::toCvShare(msg, "bgr8")->image;
 
+
     //srcImage.convertTo(srcImage, CV_32F);
 
 
-    double x;
-    double y;
-	double length;
-	double angle;
-
-
-
-    // convert to HSV color space
-    Mat hsvImage;
-    cvtColor(srcImage, hsvImage, CV_BGR2HSV);
-
-    // split the channels
-    std::vector<cv::Mat> hsvChannels;
-    split(hsvImage, hsvChannels);
-
-    // hue channels tells you the color tone, if saturation and value aren't too low.
-
-    int hueValue = 50; // green color
-    int hueRange = 15; // how much difference from the desired color we want to include to the result If you increase this value, for example a red color would detect some orange values, too.
-
-    int minSaturation = 50;
-    int minValue = 50; 
-
-    // [hue, saturation, value]
-    Mat hueImage = hsvChannels[0]; 
-
-    // is the color within the lower hue range?
-    Mat hueMask;
-    cv::inRange(hueImage, hueValue - hueRange, hueValue + hueRange, hueMask);
-
-
-
-    // if desired color is near the border of the hue space, check the other side too:
-    if (hueValue - hueRange < 0 || hueValue + hueRange > 180)
-    {
-        cv::Mat hueMaskUpper;
-        int upperHueValue = hueValue + 180; // in reality this would be + 360 instead
-        cv::inRange(hueImage, upperHueValue - hueRange, upperHueValue + hueRange, hueMaskUpper);
-
-        // add this mask to the other one
-        hueMask = hueMask | hueMaskUpper;
-    }
-    
-
-    // filter out all the pixels where saturation and value do not fit the limits:
-    Mat saturationMask = hsvChannels[1] > minSaturation;
-    Mat valueMask = hsvChannels[2] > minValue;
-
-    hueMask = (hueMask & saturationMask) & valueMask;
-
-    // perform the line detection
-    std::vector<cv::Vec4i> lines;
-    cv::HoughLinesP(hueMask, lines, 1, CV_PI / 360, 50, minLineLength, maxLineGap);
-
-    std::vector<cv::Vec4i> leftLines;
-    std::vector<cv::Vec4i> rightLines;
-
-    // draw the result as big green lines:
-    for (unsigned int i = 0; i < lines.size(); ++i)
-    {
-        cv::Vec4i line = lines[i];
-
-
-        cv::Vec2d v1(line[0], line[1]); //x1 and y1
-        cv::Vec2d v2(line[2], line[3]); //x2 and y2
-        cv::Vec2d baseVec(1, 0);
-        cv::Vec2d res = v2 - v1;
-
-        if(res[1] > 0) {
-            res = res * -1;
-        }
-
-        double dotP = res.dot(baseVec);
-
-        double angle = std::acos(dotP / cv::norm(res)) * (180/3.141592);
-
-        if(angle > 90){
-            angle = 180 - angle;
-        }
-
-        if(angle > 20 && angle < 60)
-        {
-            //cv::line(srcImage, cv::Point(lines[i][0], lines[i][1]), cv::Point(lines[i][2], lines[i][3]), cv::Scalar(0, 255, 0), 5);
-
-            if(res[0] > 0){
-                leftLines.push_back(line);
-            }
-            else{
-                rightLines.push_back(line);
-            }
-        }      
-    }
-
-    Vec4i rightMeanLine(0,0,0,0);
-    Vec4i leftMeanLine(0,0,0,0);
-    if(rightLines.size() > 0 && leftLines.size() > 0){
-        for(auto line : rightLines){
-            rightMeanLine = rightMeanLine + line;
-        }
-        rightMeanLine = rightMeanLine / (int)rightLines.size();
-
-        for(auto line : leftLines){
-            leftMeanLine = leftMeanLine + line;
-        }
-        leftMeanLine = leftMeanLine / (int)leftLines.size();
-    }
-
-    cv::line(srcImage, cv::Point(rightMeanLine[0], rightMeanLine[1]), cv::Point(rightMeanLine[2], rightMeanLine[3]), cv::Scalar(255, 0, 0), 5);
-    cv::line(srcImage, cv::Point(leftMeanLine[0], leftMeanLine[1]), cv::Point(leftMeanLine[2], leftMeanLine[3]), cv::Scalar(255, 0, 0), 5);
-
-    cv::Vec2d centerDown((leftMeanLine[0] + rightMeanLine[2]) / 2, leftMeanLine[1]);
-    cv::Vec2d centerUp((leftMeanLine[2] + rightMeanLine[0]) / 2, leftMeanLine[3]);
-
-
-    cv::Vec4i centerLine(centerDown[0],centerDown[1], centerUp[0], centerUp[1]);
-
-    cv::line(srcImage, cv::Point(centerLine[0], centerLine[1]), cv::Point(centerLine[2], centerLine[3]), cv::Scalar(0, 0, 255), 5);
-
-    cv::imshow("Green-Detection", srcImage);
-
 }
+
+
+
+PointCloud<PointXYZRGB>::Ptr CreatePointCloud(Mat depth_image, Mat color_image, vector<float> K){
+
+
+	PointCloud<PointXYZRGB>::Ptr pointcloud(new PointCloud<PointXYZRGB>);
+	pointcloud->reserve(depthImage.rows * depthImage.cols);
+   	//vector<Point3f> points;
+
+    float fx, fy, cx, cy;
+    fx = (float)K[0];
+    fy = (float)K[4];
+    cx = (float)K[2];
+    cy = (float)K[5];
+
+    //cout << cx << " | " << cy << " | " << fx << " | " << fy << endl;
+
+    for (int v = 0; v < depth_image.rows; v++){
+    	for (int u = 0; u < depth_image.cols; u++){
+    		float depth_value = depth_image.at<float>(v, u);
+
+    		PointXYZRGB p;
+    		p.x = ((u - cx)*depth_value*(1.0f / fx)) / 1000.0f;
+    		p.y = ((v - cy)*depth_value*(1.0f / fy)) / 1000.0f;
+    		p.z = (depth_value) / 1000.0f;
+    		pointcloud->points.push_back(p);
+    	}
+    }
+    return pointcloud;
+}
+
+
 void infoCallback(const sensor_msgs::CameraInfo &camera_info){
-	cout << camera_info << endl;
-	//P[0] f(x), P[2]c(x), P[5]f(y), P[6]c(y)
-	// f(x), f(y) = Focal Length
-	// c(x), x(y) = Principal Point
+
+	Mat undistortedSplit;
+	Mat undistortedDepth;
+
+	vector<float> kValues, dValues;
+	auto K = camera_info.K;
+	auto D = camera_info.D;
+	kValues.assign(K.begin(), K.end());
+	//dValues.assign(D.begin(), D.end());
+
+	if(!splitImage.empty() && !depthImage.empty() && !irImage.empty()){
+
+		//undistort(splitImage, undistortedSplit, kValues, dValues);
+		//undistort(depthImage, undistortedDepth, kValues, dValues);
+
+		auto pointcloud = CreatePointCloud(depthImage, splitImage, kValues);
+		sensor_msgs::PointCloud2 out;
+		pcl::toROSMsg(*pointcloud, out);
+		out.header.frame_id = "camera_link";
+
+		pointcloud_publisher.publish(out);
+
+	}
 
 
 }
+
+
+void depthCallback(const sensor_msgs::ImageConstPtr& msg){
+
+	depthImage = cv_bridge::toCvShare(msg, "16UC1")->image;
+
+}
+
+
+
 
 void infraCallback(const sensor_msgs::ImageConstPtr& msg){
 
@@ -214,13 +175,134 @@ void infraCallback(const sensor_msgs::ImageConstPtr& msg){
         res.convertTo(res, CV_8UC1);
 
         applyColorMap(res, newHSL, COLORMAP_JET);
-    	imshow("Infrared Image", irImage);
+
+
+		double x;
+	    double y;
+		double length;
+		double angle;
+
+
+
+	    // convert to HSV color space
+	    Mat hsvImage;
+	    cvtColor(newHSL, hsvImage, CV_BGR2HSV);
+
+	    // split the channels
+	    std::vector<cv::Mat> hsvChannels;
+	    split(hsvImage, hsvChannels);
+
+	    // hue channels tells you the color tone, if saturation and value aren't too low.
+
+	    int hueValue = 0; // green color
+	    int hueRange = 15; // how much difference from the desired color we want to include to the result If you increase this value, for example a red color would detect some orange values, too.
+
+	    int minSaturation = 50;
+	    int minValue = 50; 
+
+	    // [hue, saturation, value]
+	    Mat hueImage = hsvChannels[0]; 
+
+	    // is the color within the lower hue range?
+	    Mat hueMask;
+	    cv::inRange(hueImage, hueValue - hueRange, hueValue + hueRange, hueMask);
+
+
+
+	    // if desired color is near the border of the hue space, check the other side too:
+	    if (hueValue - hueRange < 0 || hueValue + hueRange > 180)
+	    {
+	        cv::Mat hueMaskUpper;
+	        int upperHueValue = hueValue + 180; // in reality this would be + 360 instead
+	        cv::inRange(hueImage, upperHueValue - hueRange, upperHueValue + hueRange, hueMaskUpper);
+
+	        // add this mask to the other one
+	        hueMask = hueMask | hueMaskUpper;
+	    }
+	    
+
+	    // filter out all the pixels where saturation and value do not fit the limits:
+	    Mat saturationMask = hsvChannels[1] > minSaturation;
+	    Mat valueMask = hsvChannels[2] > minValue;
+
+	    hueMask = (hueMask & saturationMask) & valueMask;
+
+	    // perform the line detection
+	    std::vector<cv::Vec4i> lines;
+	    cv::HoughLinesP(hueMask, lines, 1, CV_PI / 360, 50, minLineLength, maxLineGap);
+
+	    std::vector<cv::Vec4i> leftLines;
+	    std::vector<cv::Vec4i> rightLines;
+
+	    // draw the result as big green lines:
+	    for (unsigned int i = 0; i < lines.size(); ++i)
+	    {
+	        cv::Vec4i line = lines[i];
+
+
+	        cv::Vec2d v1(line[0], line[1]); //x1 and y1
+	        cv::Vec2d v2(line[2], line[3]); //x2 and y2
+	        cv::Vec2d baseVec(1, 0);
+	        cv::Vec2d result = v2 - v1;
+
+	        if(result[1] > 0) {
+	            result = result * -1;
+	        }
+
+	        double dotP = result.dot(baseVec);
+
+	        double angle = std::acos(dotP / cv::norm(result)) * (180/3.141592);
+
+	        if(angle > 90){
+	            angle = 180 - angle;
+	        }
+
+	        if(angle > 20 && angle < 60)
+	        {
+	            //cv::line(srcImage, cv::Point(lines[i][0], lines[i][1]), cv::Point(lines[i][2], lines[i][3]), cv::Scalar(0, 255, 0), 5);
+
+	            if(result[0] > 0){
+	                leftLines.push_back(line);
+	            }
+	            else{
+	                rightLines.push_back(line);
+	            }
+	        }      
+	    }
+
+	    Vec4i rightMeanLine(0,0,0,0);
+	    Vec4i leftMeanLine(0,0,0,0);
+	    if(rightLines.size() > 0 && leftLines.size() > 0){
+	        for(auto line : rightLines){
+	            rightMeanLine = rightMeanLine + line;
+	        }
+	        rightMeanLine = rightMeanLine / (int)rightLines.size();
+
+	        for(auto line : leftLines){
+	            leftMeanLine = leftMeanLine + line;
+	        }
+	        leftMeanLine = leftMeanLine / (int)leftLines.size();
+	    }
+
+	    cv::line(newHSL, cv::Point(rightMeanLine[0], rightMeanLine[1]), cv::Point(rightMeanLine[2], rightMeanLine[3]), cv::Scalar(255, 0, 0), 5);
+	    cv::line(newHSL, cv::Point(leftMeanLine[0], leftMeanLine[1]), cv::Point(leftMeanLine[2], leftMeanLine[3]), cv::Scalar(255, 0, 0), 5);
+
+	    cv::Vec2d centerDown((leftMeanLine[0] + rightMeanLine[2]) / 2, leftMeanLine[1]);
+	    cv::Vec2d centerUp((leftMeanLine[2] + rightMeanLine[0]) / 2, leftMeanLine[3]);
+
+
+	    cv::Vec4i centerLine(centerDown[0],centerDown[1], centerUp[0], centerUp[1]);
+
+	    cv::line(newHSL, cv::Point(centerLine[0], centerLine[1]), cv::Point(centerLine[2], centerLine[3]), cv::Scalar(0, 0, 255), 5);
+
+
+    	//imshow("Infrared Image", irImage);
     	imshow("NDVI", newHSL);
     	waitKey(30);
   	}
-
-
 }
+
+
 
 
 int main(int argc, char **argv)
@@ -233,8 +315,11 @@ int main(int argc, char **argv)
     image_transport::ImageTransport it(nh);
     image_transport::Subscriber sub = it.subscribe("camera/color/image_raw", 1, imageCallback);
     image_transport::Subscriber sub2 = it.subscribe("camera/infra1/image_rect_raw", 1, infraCallback);
-    ros::Subscriber sub3 = nh.subscribe("camera/color/camera_info", 1, infoCallback);
+    image_transport::Subscriber sub3 = it.subscribe("camera/aligned_depth_to_color/image_raw", 1, depthCallback);
+	ros::Subscriber sub4 = nh.subscribe("camera/color/camera_info", 1, infoCallback);
 
+	pointcloud_publisher = nh.advertise<sensor_msgs::PointCloud2>("myPointCloud", 1);
+    
     ros::spin();
 
     return 0;
